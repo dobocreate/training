579230f26dd52311937fb0172d13323658a37b11
dobocreate <kishida@dobocreate.jp>
2024-08-14 18:28:04 +0900

    (choi)8/14JavaScriptの課題

--- choi_javascript_todo/TODO.js
+++ choi_javascript_todo/TODO.js
@@ -1,10 +1,13 @@
 //버튼을 클릭했을때, 이벤트 발생
-document.getElementsByClassName("input-button")[0].addEventListener('click',InputText);
-
+document.getElementsByClassName("input-button")[0].addEventListener('click',InputText );
+document.getElementsByClassName("input-button")[0].addEventListener('click',Createlist); 
+let AddText;
 //검색창에 등록한 정보를 객체화 하는 함수
 function InputText(){
-     AddText = document.getElementsByClassName("text")[0].value ;
-    document.getElementsByClassName("text")[0].value = ""; //초기화
+      AddText = document.getElementsByClassName("text")[0].value ;
+     document.getElementsByClassName("text")[0].value = ""; //초기화
+     console.log("1");
+     return AddText}
 
 // const TODOlist = document.createElement('div');
 // const DoButton = document.createElement('button');
@@ -19,11 +22,58 @@ function InputText(){
 // TODOlist.appendChild(DoButton);
 
 
-const template = document.getElementById("template");
-const TODOlist = template.children[0].cloneNode(true);
- TODOlist.querySelector('.todo-text').TextContent=AddText;
+ 
+
+ let TODOlist;
+ let DeleatButton;
+
+ function Createlist()
+ {
+ const template = document.getElementsByClassName("todo-item")[0]
+ TODOlist = template.cloneNode(true)
+ 
+ const text =TODOlist.querySelector('.todo-text');
+ text.append(AddText);
+
+ DeleatButton = TODOlist.querySelector('.DeleatButton');
+ 
+
+  
+ 
  document.getElementsByClassName("TODO-area")[0].appendChild(TODOlist);
+ console.log("2");
 
 
-};
+ 
+
+ 
+ };
+ 
+
+  
+
+ let N = 0;  // 인덱스 초기화
+ const deleteButtons = document.getElementsByClassName("DeleatButton");
+ const todoItems = document.getElementsByClassName("todo-item");
+
+ deleteButtons[1].addEventListener('click',DeleatBox(todoItems[1]))
+ 
+ 
+
+ while (N < deleteButtons.length) {
+     deleteButtons[N].addEventListener('click', function() {
+         DeleatBox(todoItems[N]);
+     });
+     console.log("4");
+     N++;
+     
+ }
+ 
+ function DeleatBox(target) {
+     target.remove(); // 요소를 삭제합니다.
+     console.log("3");
+ }
+
+
+
 
