f5b4525e89900c6c9c20ad7513fa60ec85664a40
dobocreate <kishida@dobocreate.jp>
2025-02-10 23:12:25 +0900

    (岡田)戻すボタンの実装途中

--- okada_js_training/todo.js
+++ okada_js_training/todo.js
@@ -3,7 +3,6 @@
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = ""; //追加したテキストを得て空欄に
-  //ここより上の時点でうまくいってない
 
   //ここからが入力したテキストがtodoリストにのる作業
   //li生成
@@ -19,8 +18,38 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    //ボタンを削除
+    const moveTarget = completeButton.closest("li");
+    completeButton.nextElementSibling.remove();
+    completeButton.remove();
+    //戻すボタンを設置
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      moveTarget.firstElementChild.appendChild(backButton);
+    });
+    moveTarget.firstElementChild.appendChild(backButton);
+    //完了リストに移動
+    document.getElementById("complete-list").appendChild(moveTarget);
   });
+
+  // //
+  // //戻すボタンで未完了に移動
+  // backButton.addEventListener("click", () => {
+  //   const moveTarget = backButton.closest("li");
+  //   backButton.nextElementSibling.remove();
+  //   backButton.remove();
+
+  //   const completeButton = document.createElement("button");
+  //   completeButton.innerText = "完了";
+  //   const deleteButton = document.createElement("button");
+  //   deleteButton.innerText = "削除";
+  //   moveTarget.firstElementChild.appendChild(completeButton);
+  //   moveTarget.firstElementChild.appendChild(deleteButton);
+  //   //未完了リストに移動
+  //   document.getElementById("incomplete-list").appendChild(moveTarget);
+  // });
+  // //
 
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
@@ -36,6 +65,11 @@ const onClickAdd = () => {
   li.appendChild(div);
   //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(li);
+};
+
+const createIncompleteTodo = (todo) => {
+  const li = document.createElement("li");
+  const div = document.createElement("div");
 };
 
 document.getElementById("add-button").addEventListener("click", onClickAdd);
