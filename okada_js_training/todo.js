96a211e7cdedf975ce1c83d36cf795e2d44a1df2
dobocreate <kishida@dobocreate.jp>
2025-02-10 22:12:12 +0900

    (岡田)todoリスト削除タグまで実装済み

--- okada_js_training/todo.js
+++ okada_js_training/todo.js
@@ -1,8 +1,8 @@
-import "./todo.css";
+// import "./todo.css";
 
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
-  document.getElementById("add-text").value = "";
+  document.getElementById("add-text").value = ""; //追加したテキストを得て空欄に
   //ここより上の時点でうまくいってない
 
   //ここからが入力したテキストがtodoリストにのる作業
@@ -16,8 +16,23 @@ const onClickAdd = () => {
   p.className = "todo-item";
   p.innerText = inputText;
   //button生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //削除ボタンの親のliタグを削除
+    const deleteTarget = deleteButton.closest("li");
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
 
   div.appendChild(p); //階層構造にできるdivの子がp
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   li.appendChild(div);
   //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(li);
