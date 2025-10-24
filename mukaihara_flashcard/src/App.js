8d54d687510d61a3e19d6a2b11903608e45d15db
mukahiro <biz.mukaihara@gmail.com>
2025-10-24 15:22:46 +0900

    (向原) プログレスバー追加とスタイルの調節

--- mukaihara_flashcard/src/App.js
+++ mukaihara_flashcard/src/App.js
@@ -1,60 +1,69 @@
 import { useState } from "react";
 
 function App() {
-  /** ステートを定義 */
+  /** 入力フォームの表の文字 */
   const [cardFrontText, setCardFrontText] = useState("");
+  /** 入力フォームの裏の文字 */
   const [cardBackText, setCardBackText] = useState("");
+  /** カードのリスト（初期カード5枚） */
   const [cardList, setCardList] = useState([
-    { front: "表の文字" , back: "裏の文字", checked: false },
-    { front: "0" , back: "1", checked: false },
-    { front: "2" , back: "3", checked: false },
-    { front: "4" , back: "5", checked: false },
-    { front: "6" , back: "7", checked: false },
-    { front: "8" , back: "9", checked: false }
+    { front: "表 0" , back: "裏 1", checked: false },
+    { front: "表 2" , back: "裏 3", checked: false },
+    { front: "表 4" , back: "裏 5", checked: false },
+    { front: "表 6" , back: "裏 7", checked: false },
+    { front: "表 8" , back: "裏 9", checked: false }
   ])
-
+  /** 学習画面が実行中かどうか */
   const [isRunned, setIsRunned] = useState(false);
+  /** 学習画面のカードのインデックス */
   const [learningCardIndex, setLearningCardIndex] = useState(0);
 
-  /** 入力フォームの値の更新 */
+  /** 入力フォームの値が変わったらステートを更新 */
   const onChangeCardFront = (event) => setCardFrontText(event.target.value);
   const onChangeCardBack = (event) => setCardBackText(event.target.value);
 
   /** カードを追加するボタンが押されたとき */
   const onClickAddCard = () => {
-    if (cardFrontText === "" || cardBackText === "") return;
+    if (cardFrontText === "" || cardBackText === "") return; // 表もしくは裏が空文字のときは追加しない
     const newCardList = [...cardList, {front: cardFrontText, back: cardBackText, checked: false}]; // カードリストを新要素を追加
     
-    setCardFrontText("");
-    setCardBackText("");
+    setCardFrontText(""); // 入力フォームをリセット
+    setCardBackText(""); // 入力フォームをリセット
     
-    setCardList(newCardList);
+    setCardList(newCardList); // カードリストを更新
   }
 
   /** カードのチェックボタンが押されたとき */
   const onClickCardCheck = (index) => {
-    const newCardList = [...cardList];
+    const newCardList = [...cardList]; // カードリストを新規作成
 
-    newCardList[index].checked = !newCardList[index].checked; // チェックを反転
+    newCardList[index].checked = !newCardList[index].checked; // インデックスで指定したカードのチェックを反転
 
-    setCardList(newCardList);
+    setCardList(newCardList); // カードリストを更新
   }
   
-  /** 学習パネルのチェックボタンが押されたとき */
-  const onClickCheck = (index) => {
-    const newCardList = [...cardList];
+  /** 学習画面のチェックボタンが押されたとき */
+  const onClickCheck = () => {
+    const newCardList = [...cardList]; // カードリストを新規作成
 
-    newCardList[parseInt(learningCardIndex/2)].checked = !newCardList[parseInt(learningCardIndex/2)].checked; // チェックを反転
+    newCardList[parseInt(learningCardIndex/2)].checked = !newCardList[parseInt(learningCardIndex/2)].checked; /// 現在の学習画面のカードのチェックを反転
 
-    setCardList(newCardList);
+    setCardList(newCardList); // カードリストを更新
+  }
+
+  /** 現在の学習画面のカードの文字を読み上げ */
+  const onClickReadCard = () => {
+    const uttr = new SpeechSynthesisUtterance(learningCardIndex%2 ? cardList[parseInt(learningCardIndex/2)].back : cardList[parseInt(learningCardIndex/2)].front);
+    speechSynthesis.speak(uttr);
   }
 
   /** 学習パネルの起動と停止 */
-  const onClickStart = () => setIsRunned(true);
-  const onClickStop = () => setIsRunned(false);
+  const onClickStart = () => setIsRunned(true); // 学習画面をオン
+  const onClickStop = () => setIsRunned(false); // 学習画面をオフ
+  const onClickReset = () => setLearningCardIndex(0); // 現在の学習画面のカードインデックスを0にリセット
 
-  const onClickPrevCard = () =>  setLearningCardIndex(learningCardIndex > 0 ? learningCardIndex-1 : 0);
-  const onClickNextCard = () =>  setLearningCardIndex(learningCardIndex < cardList.length*2-1 ? learningCardIndex+1 : cardList.length*2-1);
+  const onClickPrevCard = () =>  setLearningCardIndex(learningCardIndex > 0 ? learningCardIndex-1 : 0); // 次のカードに移動（インデックス0のときは移動しない）
+  const onClickNextCard = () =>  setLearningCardIndex(learningCardIndex < cardList.length*2-1 ? learningCardIndex+1 : cardList.length*2-1); // 次のカードに移動（インデックス最大のときは移動しない）
 
   return (
     <>
@@ -68,33 +77,39 @@ function App() {
       <ul className="card-list">
         {cardList.map((card, index) => (
           <li key={index}>
-            <div className="card-front">
-              <span>{card.front}</span>
-            </div>
-            <div className="card-back">
-              <span>{card.back}</span>
+            <div className="card-text">
+              <div className="card-front">
+                <span>{card.front}</span>
+              </div>
+              <div className="card-back">
+                <span>{card.back}</span>
+              </div>
             </div>
             <div className="card-check">
-              <button onClick={() => onClickCardCheck(index)}>{card.checked ? "〇" : "×" }</button>
+              <button style={{backgroundColor: card.checked ? "#8080dd" : "#dd8080"}} onClick={() => onClickCardCheck(index)}>{card.checked ? "〇" : "×" }</button>
             </div>
           </li>
         ))}
       </ul>
       <div className="control-area">
-        <button onClick={onClickStop}>停止</button>
-        <button onClick={onClickStart}>開始</button>
+        <button onClick={onClickStart}>学習開始</button>
       </div>
-      { isRunned && /*学習パネルが起動中か否か */
+      { isRunned && /*学習パネルがオンか否か */
       <div className="learning-panel">
         <div className="header">
-          <button onClick={onClickStop}>＜ 戻る</button>
+          <button className="stop-btn" onClick={onClickStop}>＜ 戻る</button>
           <span>学習パネル</span>
+          <button className="reset-btn" onClick={onClickReset}>リセット</button>
         </div>
         <div className="contents">
           <button className="prev-btn" onClick={onClickPrevCard}>＜</button>
           <button className="next-btn" onClick={onClickNextCard}>＞</button>
-          <button className="check-btn" onClick={() => onClickCheck(parseInt(learningCardIndex/2))}>{cardList[parseInt(learningCardIndex/2)].checked ? "〇" : "×" }</button>
+          <button className="read-btn" onClick={onClickReadCard}>♪</button>
+          <button className="check-btn" style={{backgroundColor: cardList[parseInt(learningCardIndex/2)].checked ? "#8080dd" : "#dd8080"}} onClick={() => onClickCheck(parseInt(learningCardIndex/2))}>{ cardList[parseInt(learningCardIndex/2)].checked ? "〇" : "×" }</button>
           <span className="active-card">{learningCardIndex%2 ? cardList[parseInt(learningCardIndex/2)].back : cardList[parseInt(learningCardIndex/2)].front}</span>
+        </div>
+        <div className="footer">
+          <div className="progress"><span className="progress-bar" style={{width: learningCardIndex/(cardList.length*2-1)*100 + "%"}}>{parseInt(learningCardIndex/(cardList.length*2-1)*100)}%</span></div>
         </div>
       </div>
       }
